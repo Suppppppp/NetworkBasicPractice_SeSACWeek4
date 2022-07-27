2f00f184bea6713531211e4feb9b3cf0a0d154bf
Suppppppp <u1316@daum.net>
2022-07-27 11:42:07 +0900

    Initial Commit

---
+++ NetworkBasicPractice_SeSACWeek4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkBasicPractice_SeSACWeek4
+//
+//  Created by KYUNGSUP GO on 2022/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
