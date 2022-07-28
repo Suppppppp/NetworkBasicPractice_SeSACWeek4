0377fa13adaee2063db76fff027b4d112d46eb6c
Suppppppp <u1316@daum.net>
2022-07-28 12:44:02 +0900

    - add webkit - add function searchBar move to url

--- NetworkBasicPractice_SeSACWeek4/WebViewController.swift
+++ NetworkBasicPractice_SeSACWeek4/WebViewController.swift
@@ -13,11 +13,31 @@ class WebViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var webView: WKWebView!
     
+    var destinationURL: String = "https://www.apple.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchBar.delegate = self
+        openWebPage(url: destinationURL)
     }
 
+    func openWebPage(url:String) {
+        guard let url = URL(string: url) else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
     
 
+}
+
+extension WebViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        openWebPage(url: searchBar.text!)
+    }
 }
